f5bd01a59166de18bbde4eae7138cacdec22ca46
Nzeller15 <Zeller.niklas153112@gmail.com>
2022-11-24 20:49:54 +0100

    Text Verfassung und Nutzung von Methoden

--- Monsterkampf Simulator/Monsterkampf Simulator/Program.cs
+++ Monsterkampf Simulator/Monsterkampf Simulator/Program.cs
@@ -5,26 +5,38 @@ using System.Runtime.Intrinsics.Arm;
 
 namespace Monsterkampf_Simulator
 {
-    internal class Program
+    internal class Program 
     {
 
         static Monster monster1;      // Die zwei Kämpfenden Monster erstellen
         static Monster monster2;
+
         static void Main(string[] args)
         {
 
-            
-            // Monster als Strings Festlegen
-            // Statuswerte als Floats Initieren
 
-
-
-
-          //While Schleife für Monsterauswahl
+            Intruduction();
+            PickMonster();
+           
 
 
 
            
+        }
+        static void Intruduction() 
+        {
+            Console.WriteLine("Hello my friend");
+            Console.ReadKey();
+            Console.WriteLine("You're now able to pick two monsters, which will Fight each other");
+            Console.ReadKey();
+            Console.WriteLine("The rules are simple, you only pick two monsters and each monster has to be from another race");
+        
+        }
+        static void PickMonster() //Methode für die Monsterauswahl erstellen
+        { 
+
+
+        
         }
     }
 }
